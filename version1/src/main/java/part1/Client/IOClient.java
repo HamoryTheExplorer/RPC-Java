ca75d625bf7eea753499826d9cde4d06075a022d
Zheng_HP <2994505782@qq.com>
2026-02-28 16:31:33 +0800

    2.28
    环境配置

--- version1/src/main/java/part1/Client/IOClient.java
+++ version1/src/main/java/part1/Client/IOClient.java
@@ -19,13 +19,14 @@ public class IOClient {
     //这里负责底层与服务端的通信，发送request，返回response
     public static RpcResponse sendRequest(String host, int port, RpcRequest request){
         try {
-            Socket socket=new Socket(host, port);
-            ObjectOutputStream oos=new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream ois=new ObjectInputStream(socket.getInputStream());
+            Socket socket=new Socket(host, port); // 通过 socket 与服务端建立 TCP 连接
+            ObjectOutputStream oos=new ObjectOutputStream(socket.getOutputStream()); // 将对象序列化发送到服务端
+            ObjectInputStream ois=new ObjectInputStream(socket.getInputStream());    // 接收并反序列化对象
 
-            oos.writeObject(request);
-            oos.flush();
+            oos.writeObject(request); // 将 RpcRequest 对象序列化，并通过输出流发送到服务端
+            oos.flush();              // 刷新输出流，确保数据完全发送
 
+            // 从输入流中读取服务端返回的 序列化对象，并反序列化为 RpcResponse
             RpcResponse response=(RpcResponse) ois.readObject();
             return response;
         } catch (IOException | ClassNotFoundException e) {
--- version1/src/main/java/part1/Client/proxy/ClientProxy.java
+++ version1/src/main/java/part1/Client/proxy/ClientProxy.java
@@ -17,23 +17,26 @@ import java.lang.reflect.Proxy;
  */
 @AllArgsConstructor
 public class ClientProxy implements InvocationHandler {
-    //传入参数service接口的class对象，反射封装成一个request
+    // 传入参数service接口的class对象，反射封装成一个request
     private String host;
     private int port;
 
-    //jdk动态代理，每一次代理对象调用方法，都会经过此方法增强（反射获取request对象，socket发送到服务端）
+    // jdk动态代理，每一次代理对象调用方法，都会经过此方法增强（反射获取request对象，socket发送到服务端）
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        //构建request
+        // 构建request
         RpcRequest request=RpcRequest.builder()
                 .interfaceName(method.getDeclaringClass().getName())
                 .methodName(method.getName())
                 .params(args).paramsType(method.getParameterTypes()).build();
-        //IOClient.sendRequest 和服务端进行数据传输
+        // 与服务端进行通信，将请求发送出去，并接受 RpcResponse 响应
         RpcResponse response= IOClient.sendRequest(host,port,request);
+        // 获取服务端返回结果，并返回给调用者
         return response.getData();
     }
+    // 动态生成一个实现指定接口的代理对象
      public <T>T getProxy(Class<T> clazz){
+        // 使用 Proxy.newProxyInstance 方法动态创建一个代理对象，传入类加载器
         Object o = Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, this);
         return (T)o;
     }
